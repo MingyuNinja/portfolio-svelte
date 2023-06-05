05c89a8218bcc57c96cadba624d823a9d99a8e68
Mingyu Park <pmg1202@snu.ac.kr>
2023-06-05 20:00:57 +0900

    Debugging in process...

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,7 @@ export const projects = [
       title: "HALO (HAnaro Launch Object)",
       url: "https://www.youtube.com/watch?v=PvHXIeuu4qs",
       description: "Solid propellant rocket “HALO(HAnaro Launch Object)”. It aims to reach 10,000ft in height with a commercial off-the-shelf(COTS) motor manufactured by Cesaroni Technology, Inc.. Its main mission is to deliver the payload, which contains an inertial compensator, to the desired altitude, make a successful wireless communication via Iridium module with a ground station and achieve full recovery by deploying both of the drogue parachute and the main parachute.",
-      imageSrc: "/assets/png/HALO.png",
+      imageSrc: "./assets/png/HALO.png",
     },
 
     {
@@ -20,7 +20,7 @@ export const projects = [
       title: "Development and Demonstration of Cold Gas Thruster for Rocket Attitude Stabilization",
       url: "https://ksas.or.kr",
       description: "Cold Gas Thruster with 0.75N thrust. This project was funded by the SNU Undergraduate Research Program through the Faculty of Liberal Education, Seoul National University, and submitted to Spring Conference of the Korean Society for Aeronautical & Space Sciences, 2019",
-      imageSrc: "/assets/png/pen.png",
+      imageSrc: "./assets/png/pen.png",
     },
   ];
   
